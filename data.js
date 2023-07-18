b6cfb95d5aac0eaa0713ce22ba53bfd1439f1d42
Rea Maroga <rea.maroga@gmail.com>
2023-07-18 16:05:15 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+  {
+    name: "Come Back",
+    path: "Music/Tom Misch - Come Back.mp3",
+    artist: "Tom Misch",
+    cover: "Covers/IMG_20230629_120830.jpg",
+  },
+  {
+    name: "Scela Kuwe",
+    path: "Music/Scela-Kuwe-23-08.mp3",
+    artist: "Lerumo Badimong Cast",
+    cover: "Covers/Scela kuwe cover.png",
+  },
+  {
+    name: "Sabela",
+    path: "Music/Sabela-23.08.mp3",
+    artist: "Lerumo Badimong Cast",
+    cover: "Covers/Sabela cover.jpeg",
+  },
+  {
+    name: "Thongo Lam",
+    path: "Music/Thongo-Lam.mp3",
+    artist: "Lerumo Badimong Cast",
+    cover: "Covers/Thongo lam cover.jpeg",
+  },
+  {
+    name: "When you come back",
+    path: "Music/When-you-come-back.mp3",
+    artist: "Lerumo Badimong Cast",
+    cover: "Covers/Yehlisan cover.jpeg",
+  },
+];
---
+++ readme.md
@@ -0,0 +1,3 @@
+Music Player
+
+My first html, css and javascript personal projcet. Plays a selection of 5 songs.
